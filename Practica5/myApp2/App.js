fd34aa71ddd934ba4f60f5d0eb3437e0ab26406d
Emily Hernandez <123047464@upq.edu.mx>
2026-06-09 08:53:47 -0600

    Repaso de componentes, Perfil

--- Practica5/myApp2/App.js
+++ Practica5/myApp2/App.js
@@ -1,8 +1,11 @@
+//Zona 1: importación de archivos y componentes.
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
 import{Saludo} from './components/Saludo';
 import{Saludo2} from './components/Saludo2';
+import{Perfil} from './components/Perfil';
 
+//Zona2: Componentes
 export default function App() {
   return (
     <View style={styles.container}>
@@ -12,11 +15,14 @@ export default function App() {
       <Saludo/>
       <Text>---------------------</Text>
       <Saludo2/>
+      <Text>---------------------</Text>
+      <Perfil/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//Zona3: Estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- Practica5/myApp2/components/Saludo2.js
+++ Practica5/myApp2/components/Saludo2.js
@@ -6,7 +6,8 @@ export const Saludo2= () =>{
     return(
         <View>
             <Text>Hola RN: Componente Propio!</Text>
-            <Image source={require('../assets/wave.png')} />
+            <Image source={require('../assets/wave.png')} /> 
+            {/* Los dos puntos indican que se regresa a la carpeta anterior, en este caso a myApp2, y luego se accede a la carpeta assets para obtener la imagen wave.png, para mostrarla en el componente Saludo2. */}
             <Button title='Hola 201!'></Button>
         </View>
         
