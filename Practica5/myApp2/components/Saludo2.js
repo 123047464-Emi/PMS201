53dc908db4371a53e075e3bdea30dd3d28862715
Emily Hernandez <123047464@upq.edu.mx>
2026-06-08 19:11:40 -0600

    Practica6, componentes propios y nativos

---
+++ Practica5/myApp2/components/Saludo2.js
@@ -0,0 +1,15 @@
+import {View, Text, Image, Button} from 'react-native';
+
+
+//La funcion de flecha omra de simplificar funciones
+export const Saludo2= () =>{
+    return(
+        <View>
+            <Text>Hola RN: Componente Propio!</Text>
+            <Image source={require('../assets/wave.png')} />
+            <Button title='Hola 201!'></Button>
+        </View>
+        
+        
+    )
+}
