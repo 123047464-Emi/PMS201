5d631015f6360610c794eb9f9217ed35385b4421
Emily Hernandez <123047464@upq.edu.mx>
2026-06-09 15:33:35 -0600

    Practica7, useState, props

--- Practica5/myApp2/App.js
+++ Practica5/myApp2/App.js
@@ -1,23 +1,20 @@
 //Zona 1: importación de archivos y componentes.
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
-import{Saludo} from './components/Saludo';
-import{Saludo2} from './components/Saludo2';
+//import{Saludo} from './components/Saludo';
+//import{Saludo2} from './components/Saludo2';
 import{Perfil} from './components/Perfil';
 
 //Zona2: Componentes
+
+
+
+
+//Perfil usando props
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source ={require('./assets/wave.png')}/>
-      <Text>Hola Mundo!</Text>
-      <Text>---------------------</Text>
-      <Saludo/>
-      <Text>---------------------</Text>
-      <Saludo2/>
-      <Text>---------------------</Text>
-      <Perfil/>
-      <StatusBar style="auto" />
+      <Perfil nombre='Emily Hernández Eulogio' carrera='Ingeniería en Sistemas Computacionales' materia='Programación Móvil' cuatrimestre='9° cuatrimestre' />
     </View>
   );
 }
--- Practica5/myApp2/components/Perfil.js
+++ Practica5/myApp2/components/Perfil.js
@@ -1,15 +1,27 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+//Declaramos el estado
+import React, {useState} from 'react';
 
 
 //La funcion de flecha omra de simplificar funciones
-export const Perfil=() =>{
+export const Perfil=({nombre, carrera, materia, cuatrimestre}) =>{ // desestructuración de props, se colocan entre llaves y 
+// se asignan a variables, para poder usarlas dentro del componente Perfil.
+    const [mostrar, setMostrar]=useState(false); // se declara el estado mostrar, con su función setMostrar, y se inicializa en false.
+
     return(
         <View>
             <Text>Repaso de Componentes{'\n'} </Text>
-            <Text>Nombre : Emily Hernández Eulogio{'\n'} </Text>
-            <Text>Carrera : Ingeniería en Sistemas Computacionales {'\n'} </Text>
-            <Text>Materia : Programación Móvil{'\n'} </Text>
-            <Text>Cuatrimestre : 9° cuatrimestre {'\n'}</Text>
+            <Text>Nombre: {nombre }{'\n'} </Text>
+            {/*Renderizado Condicional, del lado derecho se pone lo que se quiere ocultar o mostrar*/}
+            {mostrar && 
+            <>
+            <Text>Carrera: {carrera } {'\n'} </Text>
+            <Text>Materia: {materia }l{'\n'} </Text>
+            <Text>Cuatrimestre: {cuatrimestre } {'\n'}</Text>
+            </> // Fragment, se usa para agrupar los elementos que se quieren mostrar u ocultar, sin necesidad de usar un View adicional.
+             }
+             <Button title="Detalles perfil" onPress={() => setMostrar(!mostrar)}/>
+            {/*OnPress se encarga de cambiar el estado mostrar, para mostrar u ocultar los detalles del perfil, se puede usar una función flecha para cambiar el estado, por ejemplo: onPress={() => setMostrar(!mostrar)}, esto hace que al presionar el botón, se cambie el estado mostrar a su valor contrario, es decir, si es false, se cambia a true, y si es true, se cambia a false.*/}
         </View>
     )
 }
